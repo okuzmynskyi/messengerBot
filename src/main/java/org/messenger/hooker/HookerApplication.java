5480f3bbc6a56830ea53ae65c83cb47a60868b7d
kuzmynskyi <kuzminsky.o@gmail.com>
2020-02-04 08:31:47 +0200

    add @env for dev and prod

--- src/main/java/org/messenger/hooker/HookerApplication.java
+++ src/main/java/org/messenger/hooker/HookerApplication.java
@@ -1,17 +1,13 @@
 package org.messenger.hooker;
 
-import lombok.var;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 
 @EnableWebMvc
-@ComponentScan
 public class HookerApplication {
 
     public static void main(String[] args) {
@@ -19,5 +15,6 @@ public class HookerApplication {
 
 
     }
+
 
 }
